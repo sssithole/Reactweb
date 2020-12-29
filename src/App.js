5182854ebba2153d8547771e5f389873a9393907
sssithole <ssithole@student.wethinkcode.co.za>
2020-12-29 07:45:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PageWrapper from './components/PageWrapper';
+import Home from './components/Pages/Home';
+function App() {
+  return (
+    <PageWrapper >
+      <Home />
+    </PageWrapper>
+    
+  );
+}
+
+export default App;
